e58ae1f15ecae6110c45c01053ab5bb8252524df
Hà Minh Hiếu <131228530+langkhachhoha@users.noreply.github.com>
2024-04-16 03:43:32 -0700

    Add files via upload

---
+++ AMO-ACO-Inference/test_model.py
@@ -0,0 +1,14 @@
+from Model.AMO import *
+from Normalize_data import *
+import torch
+from Load_data import *
+import copy 
+import numpy
+
+
+torch.manual_seed(123)
+
+
+print(torch.rand(size = (5,5)))
+print(torch.rand(size = (5,5)))
+print(torch.rand(size = (5,5)))
